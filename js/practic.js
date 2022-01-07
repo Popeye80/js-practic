e1da77a552acab8fee239808085b3154155f0e6f
Vladimir Pavlov <91782115+Popeye80@users.noreply.github.com>
2022-01-07 10:41:07 +0200

    7

--- js/practic.js
+++ js/practic.js
@@ -24,10 +24,42 @@
 // const result = Math.round(Math.random() * (max - min) + min);
 // console.log(result);
 
-const colors = ['tomato', 'teal', 'orange', 'deeppink', 'skyblue'];
-const max = colors.length - 1;
-const min = 0;
-const index = Math.round(Math.random() * (max - min) + min);
-const color = colors[index];
-console.log(color);
-document.body.style.backgroundColor = color;
+// const colors = ['tomato', 'teal', 'orange', 'deeppink', 'skyblue'];
+// const max = colors.length - 1;
+// const min = 0;
+// const index = Math.round(Math.random() * (max - min) + min);
+// const color = colors[index];
+// console.log(color);
+// document.body.style.backgroundColor = color;
+
+// const message = 'В этой строке 26 символов.';
+// console.log(message.length);
+
+// const brand = 'Samsung';
+// const normalizeBrand = brand.toLowerCase();
+// console.log(normalizeBrand);
+
+// console.log(5 && 6 && 7 && 'Hello' && 9);
+
+// console.log(5 || 7 || 8 || 10);
+
+// const salary = 15000;
+// if (salary <= 500) {
+//     console.log('Уровень1');
+//     }    else if (salary > 500 && salary <= 1500) {
+//         console.log('Уровень2');
+//     }    else if (salary > 1500 && salary < 3000) {
+//         console.log('Уровень3');
+//     }   else {
+//         console.log('Уровень4');
+//     } 
+//     console.log('Говно');
+
+
+const balance = 1000;
+const message = balance >= 0 ? 'Поз.баланс' : 'Нег.баланс';
+console.log(message);
+
+
+
+
